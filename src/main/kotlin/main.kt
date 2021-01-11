0e6c7858e7dfafed4ca017a6da38e9976317072a
Dushyant <dushyant30suthar@gmail.com>
2021-01-11 22:58:25 +0530

    Add while and do while loops

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,13 @@
 fun main(args: Array<String>) {
+/*
+* Loops are statements rather than expressions.
+*
+* Both of these are essentially same as java. */
+    do {
 
+    } while (true)
+
+    while (true) {
+
+    }
 }
