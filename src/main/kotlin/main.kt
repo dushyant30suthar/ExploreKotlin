6fe234bb1da9b82c657f73932bc5f3cc6abcdda7
Dushyant <dushyant30suthar@gmail.com>
2021-01-11 22:19:26 +0530

    Add typical when statement

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,18 @@
 fun main(args: Array<String>) {
 
+    /*
+    * When can also be used as expression.
+    *
+    * Else is mandatory while using it as an expression.*/
+    val b = when {
+        /*
+        * It just executes the matching branch. Doesn't falls through all of
+        * the available branches.*/
+        2 in 1..10 -> true
+        3 in 1..20 -> false
+        /*
+        * When can be used with else*/
+        else -> true
+    }
+
 }
