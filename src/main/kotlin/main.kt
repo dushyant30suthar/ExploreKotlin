a7ca5d2d43ea1852040bfb0c3030a8824f3c795f
Dushyant <dushyant30suthar@gmail.com>
2021-01-12 23:13:02 +0530

    Add a typical try catch and finally

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,25 @@
 fun main(args: Array<String>) {
-
+    /*
+    * Try catch in kotlin are expressions.
+    *
+    * Kotlin doesn't have checked and unchecked exceptions. */
+    val a = try {
+        readLine()!!.toInt()
+        throw NumberFormatException("Throw represents value of type Nothing")
+    }
+    /*
+    * Catch blocks are checked in order of their declarations.
+    *
+    * In java 7 or later we can put multple exceptions into single catch but not possible
+    * in kotlin
+    *
+    * Also use of both super types subtypes would result in compile time errors in java*/
+    catch (e: NumberFormatException) {
+        0
+    } catch (e: Exception) {
+        0
+    } finally {
+        /*
+        * To clean up resources*/
+    }
 }
