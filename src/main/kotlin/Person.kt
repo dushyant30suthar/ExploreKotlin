e9b7d813adc60b29e5378ed976ebb3015b890dba
Dushyant Suthar <dushyant30suthar@gmail.com>
2021-02-14 23:01:22 +0530

    Add primary constructor and init blocks

--- src/main/kotlin/Person.kt
+++ src/main/kotlin/Person.kt
@@ -6,14 +6,45 @@ file  name.
 We can define multiple public classes in single
 file.
 * */
-class Person {
-    var firstName: String = ""
-    var lastName: String = ""
+
+/*
+* Here's a class with primary constructor.
+*
+* Doesn't have body like functions/methods.
+* It's intended to have set of property initializers
+* as well as init blocks*/
+class Person(firstName : String, lastName : String) {
+    var firstName: String = firstName
+    var lastName: String = lastName
     val age: Int = 0
 
+    init {
+        print("These statements are called everytime" +
+                "when primary constructors are called.")
+    }
 
+    init {
+        print("They could be multiple in numbers and their " +
+                "execution would be in sequence of their" +
+                "defination. These statements do not have " +
+                "return statements")
+    }
+
+    init {
+        /*
+        * Sometimes initialization of properties could be
+        * complex and can't be fit into single statement
+        * thus their initialization is allowed in
+        * init blocks.*/
+
+        this.firstName = firstName
+        this.lastName = lastName
+    }
+
+    fun method1()
+    {}
 }
 
 fun main(args: Array<String>) {
-    val person = Person()
+    val person = Person("Dushyant","Suthar")
 }
