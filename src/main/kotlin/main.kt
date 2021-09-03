dea8e8657839a09ff4d19b89b5bb621d338c681b
Dushyant Suthar <dushyant30suthar@gmail.com>
2021-09-03 22:03:36 +0530

    Benifits of lateinit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,24 @@
-fun main(args: Array<String>) {
+class Content {
 
+    /*
+    * Property without lateinit */
+    var content: String? = null
+
+    /*
+    * property with lateinit. */
+    lateinit var contentSmart: String
+
+    private fun addContent() {
+        contentSmart = "Dushyant Suthar"
+    }
+
+    /*
+    * Late init is there so that we do not have to deal with nullable types. */
+    fun getSmartContentSize(): Int {
+        return contentSmart.length
+    }
+
+    fun getContentSize(): Int {
+        return content?.length ?: 0
+    }
 }
