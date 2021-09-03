2eefef54b7e9b0599e357d0eb8e849af6c563c0b
Dushyant Suthar <dushyant30suthar@gmail.com>
2021-09-03 21:32:45 +0530

    Operators related to nullables

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -26,4 +26,28 @@ fun main(args: Array<String>) {
     * We can not use iterator on nullable types.
     *
     * But it can smart cast to nonNullable type if compiler ensures by any statement in that context..*/
+
+    something = null
+
+    //Assertion
+
+    /*
+    * Forcing compiler to consider it as nonNullable. */
+    something!!.length
+
+    //Safe call operator
+    /*
+    * Only execute statement if it is nonNull.
+    *
+    * If value is null then it returns nullable type of base type..*/
+    something?.length
+
+
+    //Elvis Operator
+    /*
+    * If value is null then use this otherwise that. We can also use this operator for return and break statements. */
+
+    something ?: "Dushyant Suthar"
+
+
 }
