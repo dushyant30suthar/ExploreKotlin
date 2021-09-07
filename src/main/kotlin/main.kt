4e0f70966e9c8022313042acbd8a6d40e74e479b
Dushyant Suthar <dushyant30suthar@gmail.com>
2021-09-08 00:44:21 +0530

    Add object expression

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,17 @@
 fun main(args: Array<String>) {
+    /*
+    * Object expression.*/
+    fun midPoint(argument1: IntRange, argument2: IntRange) = object {
+        val x = (argument1.first + argument1.last) / 2
+        val y = (argument2.first + argument2.last) / 2
+    }
+
+    /*
+    * Holding it in reference*/
+    val midPoint = midPoint(1..3, 2..5)
+
+    /*
+    * Using its values through reference*/
+    print("$midPoint.x,$midPoint.y")
 
 }
