9cb682361dfa629983d550c349c835f089fab056
Dushyant Suthar <dushyant30suthar@gmail.com>
2021-09-07 23:18:40 +0530

    Add lazy property

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,26 @@
+class Example {
+    /*
+    * Assigned value on first access call.
+    *
+    * var can not be used they are immutable.*/
+    private val data: String? by lazy {
+        setData()
+    }
+
+    private fun setData(): String? {
+        return ""
+    }
+
+    fun someFunction() {
+        if (data != null) {
+            /*
+            * Smart cast is not possible */
+            data.length
+        }
+    }
+}
+
+
 fun main(args: Array<String>) {
 
 }
