9a2bc131fa1c10e6219a66affd48ca96e20f91b2
Dushyant Suthar <dushyant30suthar@gmail.com>
2021-09-13 00:00:33 +0530

    Add object expression at top level and as property

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,28 @@
+/*
+* So, If we define it at top level then it's type would not be known by compiler as
+* it is related to enclosed class or something.
+*
+* Here it would be Any*/
+val midPointPropOuter = object {
+    val x = 5
+}
+
 fun main(args: Array<String>) {
     /*
     * Object expression.*/
     fun midPoint(argument1: IntRange, argument2: IntRange) = object {
         val x = (argument1.first + argument1.last) / 2
         val y = (argument2.first + argument2.last) / 2
+    }
+
+    /*
+    * Thus it could not resolved*/
+    midPointPropOuter.x
+
+    /*
+    * We can also do this.*/
+    val midPointProp = object {
+        val x = 5
     }
 
     /*
