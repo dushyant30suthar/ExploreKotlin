7620417cb9501108eceffabf0c946f28fb5fa47a
Dushyant Suthar <dushyant30suthar@gmail.com>
2021-09-14 02:13:52 +0530

    Add functional types and pass them as arguments

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,20 @@
-fun main(args: Array<String>) {
+/*
+* Functional types*/
+val sum: (Int, Int) -> Int = { a, b -> a + b }
 
+val max: (Int, Int) -> Int = { a, b -> if (a > b) a else b }
+
+fun aggregation(values: IntArray, operation: (Int, Int) -> Int): Int {
+    var result = 0
+    for (i in 0..values.lastIndex) {
+        result = operation(result, values[i])
+    }
+    return result
+}
+
+
+fun main() {
+    val values = intArrayOf(1, 6, 7, 8, 9)
+    aggregation(values, sum)
+    aggregation(values, max)
 }
