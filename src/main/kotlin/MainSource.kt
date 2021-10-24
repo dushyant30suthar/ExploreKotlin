96239c604d45dd6ef590c58c01ddfd200affce6a
Dushyant Suthar <dushyant30suthar@gmail.com>
2021-10-24 17:14:08 +0530

    Add different ways to call lambda types

--- src/main/kotlin/MainSource.kt
+++ src/main/kotlin/MainSource.kt
@@ -12,7 +12,7 @@ val shifter: (Int) -> ((Int) -> Int) = { originalValue -> { shiftingValue -> ori
 
 /*
 * Takes operation/function and returns int*/
-val valueAt2: ((Int) -> Int) -> Int = { operation -> operation(2)}
+val valueAt2: ((Int) -> Int) -> Int = { operation -> operation(2) }
 
 
 val incrementOperation = shifter(1)
@@ -26,13 +26,34 @@ fun aggregation(values: IntArray, operation: (Int, Int) -> Int): Int {
     return result
 }
 
+fun aggregation(operation: () -> String): Int {
+    return 0
+}
+
+fun aggregation(something: String, operation: (Char) -> String): Int {
+    return 0
+}
 
 fun main() {
     val values = intArrayOf(1, 6, 7, 8, 9)
     /*
     * It is recommended to put lamda outside of paranthesis*/
-    aggregation(values) { a, b -> a + b }
+
+    /*
+    * We can put _(underscores) in place of unused lambda arguments.*/
+    aggregation(values) { _, _ -> 2 }
     aggregation(values, max)
+
+    /*
+    * You can omit ()-> when there are no parameters to pass. */
+    aggregation { "Something" }
+
+
+    /*
+    * When there's just one parameter to pass then also we can omit
+    * ()-> symbols. We can refer to passed argument by using
+    * predefined name "it". */
+    aggregation("Something") { it.toString() }
 
     println(incrementOperation(10))
 
