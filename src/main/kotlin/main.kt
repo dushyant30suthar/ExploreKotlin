c300901641978ec1c9358beeaa402fe18b4535ff
Dushyant Suthar <dushyant30suthar@gmail.com>
2022-06-25 00:23:22 +0530

    Add both forms of run

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,36 @@
 fun main(args: Array<String>) {
 
+    /*
+    * Scoped functions provide us some kind of scope where we
+    * can avoid holding object to a reference just for performing
+    * some actions or calling functions on it.
+    *
+    * There are 5 scoped functions available in kotlin
+    * */
+
+
+    /*
+    * Run function with or without context.*/
+
+    val string: String
+
+
+    /*
+    * Configuring an object and computing the result.*/
+    string.run {
+        this
+    }
+
+    /*
+    * Grouping several statements into one*/
+
+    run {
+        /*
+        * Create object*/
+
+
+        /*
+        * Call methods*/
+    }
+
 }
